c98e72bbc00e027d9a69f0b0584193c4a9d0fd4b
John Salvador <johnsalvador1120@gmail.com>
2016-08-24 20:42:44 +0800

    Add files via upload

---
+++ Arrqueue.java
@@ -0,0 +1,32 @@
+
+
+public class Arrqueue {
+    public static void main(String[] args){
+    ArrayQueue objectque = new ArrayQueue(10);
+    
+    objectque.enqueue(2);
+    objectque.enqueue(4);
+    objectque.enqueue(6);
+    objectque.enqueue(8);
+    objectque.enqueue(10);
+    objectque.enqueue(12);
+    objectque.enqueue(14);
+    objectque.enqueue(16);
+    objectque.enqueue(18);
+    objectque.enqueue(20);
+    
+    objectque.showQueue();
+    System.out.println();
+    System.out.println("PeekRear: " + objectque.peekRear());
+    System.out.println("PeekFront: " + objectque.peekFront());
+    
+    System.out.println();
+    objectque.enqueue(22);
+    
+    System.out.println();
+    objectque.dequeue();
+    
+    System.out.println();
+    objectque.showQueue();
+}
+}
